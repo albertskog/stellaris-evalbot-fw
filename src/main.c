9d5c4d7de838b65f797c30f3e10b4a479c6018ac
Albert Skog <mail@albertskog.se>
2021-09-30 08:59:49 +0200

    cleanup

--- src/main.c
+++ src/main.c
@@ -1,53 +1,10 @@
-//*****************************************************************************
-//
-// project.c - Simple project to use as a starting point for more complex
-//             projects.
-//
-// Copyright (c) 2013 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-//   Redistribution and use in source and binary forms, with or without
-//   modification, are permitted provided that the following conditions
-//   are met:
-// 
-//   Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-// 
-//   Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the  
-//   distribution.
-// 
-//   Neither the name of Texas Instruments Incorporated nor the names of
-//   its contributors may be used to endorse or promote products derived
-//   from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// This is part of revision 10636 of the Stellaris Firmware Development Package.
-//
-//*****************************************************************************
 
 #include "inc/lm3s9b92.h"
-#include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
-#include "driverlib/debug.h"
-#include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
 
 #include "evalbot_drivers/io.h"
 #include "evalbot_drivers/motor.h"
-#include "evalbot_drivers/display96x16x1.h"
 #include "evalbot_drivers/sensors.h"
 
 #include "lib/millis.h"
@@ -92,11 +49,7 @@ static void state_obstacle_left(robot_data_t * state);
 static void state_obstacle_right(robot_data_t * state);
 static void state_obstacle_front(robot_data_t * state);
 
-//*****************************************************************************
-//
-// The error routine that is called if the driver library encounters an error.
-//
-//*****************************************************************************
+// Driverlib error handler
 #ifdef DEBUG
 void __error__(char *pcFilename, unsigned long ulLine)
 {
@@ -105,14 +58,13 @@ void __error__(char *pcFilename, unsigned long ulLine)
 
 int main(void)
 {
-    /* Set the clocking to run from the PLL at 50 MHz */
     SysCtlClockSet( SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ );
 
     millis_init();
     LEDsInit();
+    PushButtonsInit();
     BumpSensorsInit();
     MotorsInit();
-    PushButtonsInit();
 
     robot_data_t state = {0};
     state.current = STATE_FORWARD;
